82a2c454b13c6f99d83863fc3704be35a0f0b706
simonelopess <sisilopes20@gmail.com>
2020-08-31 16:07:44 -0300

    hash password criado

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,10 +1,13 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
 
 class User extends Model {
   static init(sequelize) {
     super.init({
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -12,6 +15,13 @@ class User extends Model {
         sequelize,
       }
     );
+    this.addHook('beforeSave', async (user)=>{
+      if(user.password){
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
 
